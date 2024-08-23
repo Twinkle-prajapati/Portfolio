7ec0e8f128432a54a4b56e6ceaf242ee976bef50
twinkle2808 <twinkleprajapati288@gmail.com>
2024-08-23 15:44:09 +0530

    "second commit"

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import '../Styles/Footer.css';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import PhoneIcon from '@mui/icons-material/Phone';
@@ -9,39 +10,39 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-content">
-        <div className="footer-contact">
-          <div className="contact-item">
-            <i className="fas fa-home"><LocationOnIcon/></i>
-            <span>Ahmedabad, India</span>
-          </div>
-          <div className="contact-item">
-          <a href="tel:+919328950167" className="contact-link">
-              <PhoneIcon/>
-              <span className='ms-2'>+91 9328950167</span>
-            </a>
-          </div>
-          <div className="contact-item">
-          <a href="mailto:twinkleprajapati288@gmail.com" className="contact-link">
-              <EmailIcon />
-              <span className='ms-2'>twinkleprajapati288@gmail.com</span>
-            </a>
-          </div>
-        </div>
-        <div className="footer-social">
-          <p>&copy; 2024 | Twinkle Prajapati</p>
-          <div className="social-icons">
-          <a href="https://www.linkedin.com/in/twinkle-prajapati-657466268/" target="_blank" rel="noopener noreferrer">
-              <LinkedInIcon className='fs-2' />
-            </a>
-            <a href="https://github.com/Twinkle-prajapati" target="_blank" rel="noopener noreferrer">
-              <GitHubIcon className='fs-2' />
-            </a>
-          </div>
-        </div>
-      </div>
+      <Container>
+        <Row className="footer-content">
+          <Col xs={12} md={6} lg={4} className="footer-contact">
+            <div className="contact-item">
+              <LocationOnIcon className="contact-icon" />
+              <span>Ahmedabad, India</span>
+            </div>
+            <div className="contact-item">
+              <a href="tel:+919328950167" className="contact-link">
+                <PhoneIcon className="contact-icon" />
+                <span>+91 9328950167</span>
+              </a>
+            </div>
+            <div className="contact-item">
+              <a href="mailto:twinkleprajapati288@gmail.com" className="contact-link">
+                <EmailIcon className="contact-icon" />
+                <span>twinkleprajapati288@gmail.com</span>
+              </a>
+            </div>
+          </Col>
+          <Col xs={12} md={6} lg={4} className="footer-social">
+            <p>&copy; 2024 | Twinkle Prajapati</p>
+            <div className="social-icons">
+              <a href="https://www.linkedin.com/in/twinkle-prajapati-657466268/" target="_blank" rel="noopener noreferrer">
+                <LinkedInIcon className="social-icon" style={{ fontSize: 34 }}  />
+              </a>
+              <a href="https://github.com/Twinkle-prajapati" target="_blank" rel="noopener noreferrer">
+                <GitHubIcon className="social-icon" style={{ fontSize: 32 }}  />
+              </a>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </footer>
   );
 }
-
-
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,46 +1,98 @@
-import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import img from '../images/image1.jpg';
-import '../Styles/Home.css';
+import React, { useState } from "react";
+import { Navbar, Nav, Container, Offcanvas } from "react-bootstrap";
+import "../Styles/Home.css";
 import { Link } from 'react-scroll';
 
 const Home = () => {
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
+
+  const handleToggle = () => {
+    setShowOffcanvas(!showOffcanvas);
+  };
+
+  const closeOffcanvas = () => {
+    setShowOffcanvas(false);
+
+  };
+
   return (
-    <>
-    <div className="hero-container">
-      <div
-        className="hero-background"
-        style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.8)), url(${img})` }}
-      >
-        <Navbar expand="lg" variant="light" className="custom-navbar">
+    <div className="home-container" id="home">
+      <div className="home-background">
+        <Navbar expand="lg" variant="light" className="navbar-custom">
           <Container>
-            <h2 className="navbar-brand-custom">My Portfolio</h2>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="nav-custom pt-2">
-                <Nav.Link className="nav-link-custom" href='#home'>Home</Nav.Link>
-                <Nav.Link className="nav-link-custom" href='#projects'>Projects</Nav.Link>
-                <Nav.Link className="nav-link-custom" href='#skills'>Skills</Nav.Link>
-                <Nav.Link className="nav-link-custom" href='#about'>About Me</Nav.Link>
-                <Nav.Link className="nav-link-custom" href='#contact'>Contact</Nav.Link>
+            <Navbar.Brand className="text-white mt-3">MY PORTFOLIO</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbar-basic" onClick={handleToggle} className="d-lg-none" />
+            <Navbar.Collapse id="navbar-basic" className="d-none d-lg-flex">
+              <Nav className="ms-auto">
+                <Nav.Link as={Link} to="home" smooth={true} duration={500} className="nav-link-custom">
+                  Home
+                </Nav.Link>
+                <Nav.Link as={Link} to="projects" smooth={true} duration={500} className="nav-link-custom">
+                  Projects
+                </Nav.Link>
+                <Nav.Link as={Link} to="skills" smooth={true} duration={500} className="nav-link-custom">
+                  Skills
+                </Nav.Link>
+                <Nav.Link as={Link} to="about" smooth={true} duration={500} className="nav-link-custom">
+                  About Me
+                </Nav.Link>
+                <Nav.Link as={Link} to="contact" smooth={true} duration={500} className="nav-link-custom">
+                  Contact
+                </Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
+
+        <Navbar.Offcanvas
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+          placement="end"
+          show={showOffcanvas}
+          onHide={closeOffcanvas}
+          className="offcanvas-custom d-lg-none"
+
+        >
+          <Offcanvas.Header closeButton>
+            <Navbar.Brand className="text-white mt-3">MY PORTFOLIO</Navbar.Brand>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="flex-column">
+              <Nav.Link as={Link} to="home" smooth={true} duration={500} className="nav-link-custom" onClick={closeOffcanvas}>
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="projects" smooth={true} duration={500} className="nav-link-custom" onClick={closeOffcanvas}>
+                Projects
+              </Nav.Link>
+              <Nav.Link as={Link} to="skills" smooth={true} duration={500} className="nav-link-custom" onClick={closeOffcanvas}>
+                Skills
+              </Nav.Link>
+              <Nav.Link as={Link} to="about" smooth={true} duration={500} className="nav-link-custom" onClick={closeOffcanvas}>
+                About Me
+              </Nav.Link>
+              <Nav.Link as={Link} to="contact" smooth={true} duration={500} className="nav-link-custom" onClick={closeOffcanvas}>
+                Contact
+              </Nav.Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+
+        <div className={`gradient-overlay ${showOffcanvas ? 'show' : ''}`} />
+
+        <div className="bio-section">
+          <h1 className="text-center">I'm Twinkle Prajapati</h1>
+          <p className="text-center btn-desig">FRONT-END DEVELOPER</p>
+          <div className="bio-btn text-center">
+            <Link to="projects" smooth={true} duration={500}>
+              <button className="mx-2 btn-one">PROJECTS</button>
+            </Link>
+            <Link to="contact" smooth={true} duration={500}>
+              <button className="mx-2 btn-two">CONTACT</button>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
-
-    <div id='home' className="bio-section text-center">
-      <h1>I'm Twinkle Prajapati</h1>
-      <p className='text-uppercase'>Front-end developer</p>
-      <div className="bio-btn text-center mt-3">
-
-      <Link to="projects"><button className='text-uppercase mx-2 btn-one'>Projects</button></Link>
-      <Link to="contact"> <button className='text-uppercase mx-2 btn-two' href='#contact'>Contact</button></Link>
-      </div>
-
-    </div>
-    </>
   );
 };
 
